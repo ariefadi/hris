35331a1f96e406b09ddc0ec9029dfab16f92bdeb
Arief Dwi Cahyo Adi <arief.adi94@gmail.com>
2025-09-17 10:14:50 +0700

    Update settings.py
    to deploy on server

--- hris/settings.py
+++ hris/settings.py
@@ -36,8 +36,9 @@ GOOGLE_AD_MANAGER_KEY_FILE = os.getenv('GOOGLE_AD_MANAGER_KEY_FILE', '')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+# DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'kiwipixel.com', 'www.kiwipixel.com']
 # Application definition
 
 INSTALLED_APPS = [
