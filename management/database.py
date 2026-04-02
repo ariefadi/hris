dfe80f0cfe3faf7a9a566bacf2dc846eb65a4645
ariefadi <arief.adi94@gmail.com>
2026-04-02 18:41:33 +0700

    commit

--- management/database.py
+++ management/database.py
@@ -218,7 +218,7 @@ class data_mysql:
                 print(f"Invalid HRIS_DB_PORT value '{raw_port}', defaulting to 3306")
                 port = 3306
             user = os.getenv('DB_USER', 'root')
-            password = os.getenv('DB_PASSWORD', 'hris123456')
+            password = os.getenv('DB_PASSWORD', '')
             database = os.getenv('DB_NAME', 'hris_trendHorizone')
 
             self.db_hris = pymysql.connect(
